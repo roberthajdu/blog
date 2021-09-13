c61dd6a8ab589df3eafc31c7854ed94b040b3a7e
Róbert Hajdu <hajdurobi19@gmail.com>
2021-09-13 20:51:14 +0200

    added bootstrap

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServicesComponent } from './services/services.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ServicesComponent
   ],
   imports: [
     CommonModule,
