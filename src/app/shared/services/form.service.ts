fa9b3f6416286b605dc3952fc9c91fdcd90911d2
Róbert Hajdu <hajdurobi19@gmail.com>
2021-09-14 22:03:38 +0200

    .

--- src/app/shared/services/form.service.ts
+++ src/app/shared/services/form.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -9,16 +9,15 @@ export class FormService {
 
   constructor(private readonly http: HttpClient) { }
 
-  submitForm(): Observable<any> {
-    return this.http.post('/', this.encode({ 'name': 'lol'}), {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" }
+  submitForm(formData: any): Observable<any> {
+    const body = new HttpParams()
+      .set('form-name', 'contact')
+      .append('name', formData.name)
+      .append('email', formData.email)
+      .append('service', formData.service)
+      .append('message', formData.message)
+    return this.http.post('/', body.toString(), {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
     });
   }
-
-  encode(data: any): string {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  }
-
 }
