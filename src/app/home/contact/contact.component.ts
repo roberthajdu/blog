e3752235ae943aff18fad92bd541bd88a63bcbe9
Róbert Hajdu <hajdurobi19@gmail.com>
2021-09-14 22:00:59 +0200

    .

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { throwError } from 'rxjs';
 import { FormService } from 'src/app/shared/services/form.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { FormService } from 'src/app/shared/services/form.service';
 })
 export class ContactComponent implements OnInit {
 
-
+  isSubmitted: boolean = false;
   contactForm = new FormGroup({
     name: new FormControl(''),
     email: new FormControl(''),
@@ -19,37 +20,13 @@ export class ContactComponent implements OnInit {
   });
 
   constructor(
-    private http: HttpClient
+    private readonly formService: FormService
   ) {};
 
   onSubmit() {
-    const body = new HttpParams()
-    .set('form-name', 'contact')
-    .append('name', this.contactForm.value.name)
-    .append('email', this.contactForm.value.email)
-    .append('service', this.contactForm.value.service)
-    .append('message', this.contactForm.value.message)
-    this.http.post('/', body.toString(), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}).subscribe(
-      res => {},
-      err => {
-        if (err instanceof ErrorEvent) {
-          //client side error
-          alert("Something went wrong when sending your message.");
-          console.log(err.error.message);
-        } else {
-          //backend error. If status is 200, then the message successfully sent
-          if (err.status === 200) {
-            alert("Your message has been sent!");
-          } else {
-            alert("Something went wrong when sending your message.");
-            console.log('Error status:');
-            console.log(err.status);
-            console.log('Error body:');
-            console.log(err.error);
-          };
-        };
-      }
-    );
+    this.formService.submitForm(this.contactForm.value).subscribe(_ => {
+      this.isSubmitted = true;
+    })
   };
 
   ngOnInit() {
