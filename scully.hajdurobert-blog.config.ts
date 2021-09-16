f1a5ecd2cd67e1b9793df4a63263022a8a21ed13
Róbert Hajdu <hajdurobi19@gmail.com>
2021-09-16 12:19:23 +0200

    gtag

--- scully.hajdurobert-blog.config.ts
+++ scully.hajdurobert-blog.config.ts
@@ -1,4 +1,13 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { prod, ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { GoogleAnalytics } from '@scullyio/scully-plugin-google-analytics';
+
+const defaultPostRenderers = [];
+
+if (prod) {
+  setPluginConfig(GoogleAnalytics, { globalSiteTag: 'UA-#########-1' });
+  defaultPostRenderers.push(GoogleAnalytics);
+}
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "hajdurobert-blog",
